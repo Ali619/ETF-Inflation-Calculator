40185ef67ed5460138a049f2b95d32541a5344f7
Name <a.bahadorani@outlook.com>
2024-01-31 12:54:02 +0330

    Edit Create_Label() class. Complete and edit Create_Entry() class. Also create a loop to create all the labels and entries for each ETFs

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from buttons import buttons
 
 class Create_Label():
-    def __init__(self, window, text, row, column):
+    def __init__(self, window: tk.Tk, text: str, row: int, column: int):
         self.text = text
         self.window = window
         self.row = row
@@ -10,12 +11,22 @@ class Create_Label():
         self.label.grid(row=self.row, column=self.column)
 
 class Create_Entry():
-    def __init__(self, fa_name: str, en_name: str, window: tk.Tk):
-        self.fa_name = fa_name
-        self.en_name = en_name
+    def __init__(self, window: tk.Tk, row: int, column: int):
         self.window = window
+        self.row = row
+        self.column = column
         self.entry = tk.Entry(self.window)
+        self.entry.grid(row=self.row, column=self.column)
     
 window = tk.Tk()
-label = Create_Label(window=window, text="label", row=0, column=0)
-window.mainloop()
+
+etf_name_label = Create_Label(window=window, text="نام صندوق\n", row=0, column=00)
+last_price_label = Create_Label(window=window, text="قیمت آخرین معامله\n", row=0, column=1)
+nav_price_label = Create_Label(window=window, text="ابطال NAV قیمت\n", row=0, column=2)
+                   
+i = 1
+for k, v in buttons.items():
+    Create_Label(window=window, text=buttons[k]["name"], row=i, column=0)
+    buttons[k]["entry"].append(Create_Entry(window, row=i, column=1))
+    buttons[k]["entry"].append(Create_Entry(window, row=i, column=2))
+    i += 1
