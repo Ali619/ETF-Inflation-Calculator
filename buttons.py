b6fb7b0299229198f7e94e27f8eeef32bf0dc16c
Name <a.bahadorani@outlook.com>
2024-01-31 13:44:56 +0330

    Remove 'etf_price' key while there is no need for it

--- buttons.py
+++ buttons.py
@@ -1,61 +1,52 @@
 buttons = {
     "tala": {
         "name": "طلا",
-        "etf_price":(),
         "entry": []
     },
 
     "zar": {
         "name": "زر",
-        "etf_price":(),
         "entry": []
     },
 
     "ayar": {
         "name": "عیار",
-        "etf_price":(),
         "entry": []
 
     },
 
     "gohar": {
         "name": "گوهر",
-        "etf_price":(),
         "entry": []
 
     },
 
     "mesghal": {
         "name": "مثقال",
-        "etf_price":(),
         "entry": []
 
     },
 
     "alton": {
         "name": "آلتون",
-        "etf_price":(),
         "entry": []
 
     },
 
     "javaher": {
         "name": "جواهر",
-        "etf_price":(),
         "entry": []
 
     },
 
     "ganj": {
         "name": "گنج",
-        "etf_price":(),
         "entry": []
 
     },
 
     "tabesh": {
         "name": "تابش",
-        "etf_price":(),
         "entry": []
 
     }
