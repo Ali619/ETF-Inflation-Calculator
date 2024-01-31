7d6fd0e110e731d86671f9a6246a1226884d7ffc
Name <a.bahadorani@outlook.com>
2024-01-31 15:37:04 +0330

    Change and refactor calculator() function to be compatible with the new flow (OOP)

--- calculator.py
+++ calculator.py
@@ -1,24 +1,22 @@
 import tkinter as tk
-from tkinter_ui import window, etfs
+from tkinter_ui import window
+from buttons import buttons
 
 def calculator():
-    for k,v in etfs.items():
-        if etfs[k]["etf_start"].get() and etfs[k]["etf_stop"].get(): 
+    for k,v in buttons.items(): 
             try:
-                result = (int(etfs[k]["etf_start"].get()) - int(etfs[k]["etf_stop"].get())) / int(etfs[k]["etf_stop"].get()) * 100
+                last_price, nav_price = int(buttons[k]["entry"][0].get()), int(buttons[k]["entry"][1].get())
+                result = (int(last_price) - int(nav_price)) / int(nav_price) * 100
                 if 0.0 <= result < 7.0:
-                    tk.Label(window, text=f"  {result:.2f} %  ", fg="green").grid(row=etfs[k]["row_number"], column=3)
+                    tk.Label(window, text=f"  {result:.2f} %  ", fg="green").grid(row=buttons[k]["row_number"], column=3)
                 if 7.01 <= result < 15.0:
-                    tk.Label(window, text=f"  {result:.2f} %  ", fg="orange").grid(row=etfs[k]["row_number"], column=3)
+                    tk.Label(window, text=f"  {result:.2f} %  ", fg="orange").grid(row=buttons[k]["row_number"], column=3)
                 if 15.01 <= result:
-                    tk.Label(window, text=f"  {result:.2f} %  ", fg="red").grid(row=etfs[k]["row_number"], column=3)
-
+                    tk.Label(window, text=f"  {result:.2f} %  ", fg="red").grid(row=buttons[k]["row_number"], column=3)
             except Exception as e:
+                tk.Label(window, text=f" "*20).grid(row=buttons[k]["row_number"], column=3)
                 print(e)
                 pass
-
-        elif not etfs[k]["etf_start"].get() or not etfs[k]["etf_stop"].get():
-            tk.Label(window, text=f" "*20).grid(row=etfs[k]["row_number"], column=3)
-
-calc_infl_button = tk.Button(window, text="اجرا", command=calculator, borderwidth=5, width=20, fg="red").grid(row=9, column=0, columnspan=2)
-exit_button = tk.Button(window, text="خروج", command=exit, borderwidth=5, width=20, fg="red").grid(row=9, column=2, columnspan=3)
+        
+calc_infl_button = tk.Button(window, text="اجرا", command=calculator, borderwidth=5, width=20, fg="red").grid(row=len(buttons)+1, column=0, columnspan=2)
+exit_button = tk.Button(window, text="خروج", command=exit, borderwidth=5, width=20, fg="red").grid(row=len(buttons)+1, column=2, columnspan=3)
