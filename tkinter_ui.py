922d674563e4a661ea9a76afd5be2365b381ffe8
Name <a.bahadorani@outlook.com>
2024-02-06 13:16:00 +0330

    Change last_price label to a better label (better explanation)

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -21,7 +21,7 @@ class Create_Entry():
 window = tk.Tk()
 window.title("حباب سنج")
 etf_name_label = Create_Label(window=window, text="نام صندوق", row=0, column=00)
-last_price_label = Create_Label(window=window, text="قیمت آخرین معامله", row=0, column=1)
+last_price_label = Create_Label(window=window, text="قیمت پایانی", row=0, column=1)
 nav_price_label = Create_Label(window=window, text="ابطال NAV قیمت", row=0, column=2)
 result_label = Create_Label(window=window, text="میزان حباب (%)", row=0, column=3)            
 
