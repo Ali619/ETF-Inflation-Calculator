6faccadb4a516a2a0b8f0740c6fa2e122f702114
Name <a.bahadorani@outlook.com>
2024-02-24 01:01:40 +0330

    Fix buttons sizes

--- calculator.py
+++ calculator.py
@@ -34,6 +34,6 @@ def save_as_text():
     with open(f"{JalaliDate.today()}.txt", 'w', encoding='utf-8') as f:
         f.write(tabulate(result, headers = 'keys', tablefmt = 'psql', numalign="center", stralign="center"))
 
-calc_infl_button = tk.Button(window, text="اجرا", command=lambda: calculator(), borderwidth=5, width=25, fg="red").grid(row=len(etfs)+1, column=0, columnspan=2)
-save_as_text_button = tk.Button(window, text="ذخیره کردن اطلاعات", command=lambda: save_as_text(), borderwidth=5, width=15, fg="red").grid(row=len(etfs)+1, column=2)
-exit_button = tk.Button(window, text="خروج", command=lambda: exit(), borderwidth=5, width=15, fg="red").grid(row=len(etfs)+1, column=3)
+calc_infl_button = tk.Button(window, text="اجرا", command=lambda: calculator(), borderwidth=5, width=25, fg="green").grid(row=len(etfs)+1, column=0, columnspan=2)
+save_as_text_button = tk.Button(window, text="ذخیره کردن اطلاعات", command=lambda: save_as_text(), borderwidth=5, width=15, fg="blue").grid(row=len(etfs)+1, column=2, columnspan=1)
+exit_button = tk.Button(window, text="خروج", command=lambda: exit(), borderwidth=5, width=10, fg="red").grid(row=len(etfs)+1, column=3, columnspan=1)
